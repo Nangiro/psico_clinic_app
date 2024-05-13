20774ae519f678ba52a995880a50385c3005e3da
daniel.abade@perallis.com <daniel.asa11@hotmail.com>
2024-05-13 14:22:34 -0300

    init do projeto
    init do projeto

---
+++ front/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='w-screen h-screen bg-black text-red-400'>
+        teste
+      </div>
+    </>
+  )
+}
+
+export default App
