3cb39a74fabe1f813bf60144571cc817255a7161
GuGaCacau <gugacaca2@gmail.com>
2024-05-19 21:07:41 -0300

    criação das models

---
+++ back/app/Http/Controllers/ScheduleHistoryController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\ScheduleHistory;
+
+class ScheduleHistoryController extends Controller
+{
+    //
+}
---
+++ back/app/Http/Controllers/SessionHistoryController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\SessionHistory;
+
+class SessionHistoryController extends Controller
+{
+    //
+}
