e4a45f4ff7c6c7077b0742a6ffc2d961225f263d
GuGaCacau <gugacaca2@gmail.com>
2024-05-27 20:14:32 -0300

    adicionando email e controlador inicial do envio do formulário de contato

---
+++ back/resources/views/mail/form.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <div>
+        <div style="border-top: 1px solid rgba(0,0,0,0.05);">
+            <div style="padding-left:30px ;padding-top: 60px; border-top: 1px solid rgba(0,0,0,0.05);text-align: left;">
+                EMAIL TESTE DE FORMULÁRIO DE CONTATO
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
